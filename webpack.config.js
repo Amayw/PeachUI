ff443481444f76d00dc9d561924cac3b14961cb8
wywang <727936199@qq.com>
2021-03-02 20:50:39 +0800

    webpack配置resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path=require('path');
-
+const HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
     mode:'production',
     entry:{
         peachui:'./lib/index.tsx'
+    },
+    resolve: {
+        extensions: ['.ts','.js','.tsx','.jsx']
     },
     output:{
         path:path.resolve(__dirname,'dist/lib'),
@@ -17,5 +20,11 @@ module.exports={
                 loader:'awesome-typescript-loader'
             }
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'PeachUI - React',
+            template:'index.html'
+        })
+    ]
 }
