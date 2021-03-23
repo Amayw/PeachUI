f131135704698bf8524cc9107b1aa0fad0cc8d74
wywang <727936199@qq.com>
2021-03-23 21:49:23 +0800

    创建icon目录，将icon组件和icon.scss放到这个目录中，之后关于icon的测试也都写到这个目录里面

---
+++ lib/icon/Icon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './svg';
+import './icon.scss';
+import classes from '../helpers/classes';
+
+interface IconProps extends React.SVGAttributes<SVGElement>{
+    name:string
+}
+
+const Icon:React.FC<IconProps>=({className,name,...restProps})=>{
+    return (
+            <svg className={classes('icon', className)} {...restProps}>
+                <use xlinkHref={'#'+name}/>
+            </svg>
+    )
+}
+
+export default Icon;
