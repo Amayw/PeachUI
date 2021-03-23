04de5bbcfb5b5e3a15d84963363be7a4bb42f2e6
wywang <727936199@qq.com>
2021-03-23 16:55:50 +0800

    创建icon.scss，Icon组件中成功使用scss

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './svg'
-
+import './icon.scss'
 interface IconProps {
     name:string
 }
 const Icon:React.FC<IconProps>=(props)=>{
     return (
-        <div>
-            <svg>
+            <svg className='icon'>
                 <use xlinkHref={`#${props.name}`} />
             </svg>
-        </div>
     )
 }
 
