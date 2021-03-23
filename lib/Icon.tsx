4ef012adde1b113e0a3035bdef16ce9c49757746
wywang <727936199@qq.com>
2021-03-23 17:32:55 +0800

    给Icon添加点击事件

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './svg'
 import './icon.scss'
 interface IconProps {
-    name:string
+    name:string,
+    onClick?:React.MouseEventHandler<SVGElement>
 }
 const Icon:React.FC<IconProps>=(props)=>{
     return (
-            <svg className='icon'>
+            <svg className='icon' onClick={props.onClick}>
                 <use xlinkHref={`#${props.name}`} />
             </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './Icon'
-ReactDOM.render(<Icon name='i-tip' />,document.querySelector('#root'));
+
+const fn:React.MouseEventHandler=(e)=>{
+    console.log(e.target)
+}
+ReactDOM.render(<Icon name='i-tip' onClick={fn}/>,document.querySelector('#root'));
