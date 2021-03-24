92af8c4ecd6827a86618baa5ba81dd339fdbb749
wywang <727936199@qq.com>
2021-03-24 15:06:21 +0800

    完成Icon的点击事件

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,6 +1,7 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../Icon'
+import {mount} from 'enzyme'
 
 describe("icon",()=>{
     it('可以接收name',()=>{
@@ -9,6 +10,9 @@ describe("icon",()=>{
     })
 
     it('可以被点击',()=>{
-
+        const fn=jest.fn();
+        const component=mount(<Icon name='left' onClick={fn}/>);
+        component.find('svg').simulate('click');
+        expect(fn).toBeCalled();
     })
 })
