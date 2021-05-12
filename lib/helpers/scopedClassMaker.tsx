c6e7124aa842866f2681f6e990238882c3e00aa5
wywang <727936199@qq.com>
2021-05-12 15:09:19 +0800

    创建高阶函数scopedClassMaker，以便生成类名

---
+++ lib/helpers/scopedClassMaker.tsx
@@ -0,0 +1,6 @@
+function scopedClassMaker(prefix:string){
+    return function scopedClass(name?:string){
+        return [prefix,name].filter(Boolean).join('-');
+    }
+}
+export default scopedClassMaker;
