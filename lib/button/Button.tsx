5c7c179c1d05dda8aaf3cf9914aa531cca1edea4
wywang <727936199@qq.com>
2021-05-21 15:29:06 +0800

    给Button组件封装点击事件

--- lib/button/Button.tsx
+++ lib/button/Button.tsx
@@ -6,14 +6,19 @@ import './button.scss'
 interface ButtonProps{
     type:'default'|'success'|'error'|'info'|'primary'|'theme'|'warn',
     loading?:boolean,
-    simple?:boolean
+    simple?:boolean,
+    onClick?: React.MouseEventHandler
 }
 
 const sc=scopedClassMaker('peachui-button');
 const Button:React.FC<ButtonProps>=(props)=>{
-
+    const onClickEvent:React.MouseEventHandler=(e)=>{
+        if(props.onClick){
+            props.onClick(e)
+        }
+    }
     return (
-        <button className={classes(sc(),sc(`${props.type}${props.simple?'-simple':''}`))}>{props.children}</button>
+        <button className={classes(sc(),sc(`${props.type}${props.simple?'-simple':''}`))} onClick={onClickEvent}>{props.children}</button>
     )
 }
 
