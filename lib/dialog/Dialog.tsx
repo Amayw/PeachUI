f6822edeb87c6f27c85f3b111d1faf26aecb51ad
wywang <727936199@qq.com>
2021-05-21 16:10:52 +0800

    完成Dialog的点击事件

--- lib/dialog/Dialog.tsx
+++ lib/dialog/Dialog.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import {Button, Icon} from '../index';
+import React, {ReactElement} from 'react';
+import {Icon} from '../index';
 import scopedClassMaker from '../helpers/scopedClassMaker';
 import './dialog.scss'
 
 const sc=scopedClassMaker('peachui-dialog');
 
 interface dialogProps {
-    visible:Boolean
+    visible:Boolean,
+    buttons:Array<ReactElement>,
+    onClose:React.MouseEventHandler,
+    closeOnClickMask?:Boolean
 }
 const Dialog:React.FC<dialogProps>=(props)=>{
+    const onClickClose:React.MouseEventHandler=(e)=>{
+        props.onClose(e);
+    }
+    const onClickMaskClose:React.MouseEventHandler=(e)=>{
+        if(props.closeOnClickMask){
+            props.onClose(e);
+        }
+    }
+
     return(
         props.visible?
         <>
-            <div className={sc('mask')}/>
+            <div className={sc('mask')} onClick={onClickMaskClose}/>
             <div className={sc()}>
-                <div className={sc('close')}>
+                <div className={sc('close')} onClick={onClickClose}>
                     <Icon name="close"/>
                 </div>
                 <header className={sc('header')}>
@@ -24,12 +36,9 @@ const Dialog:React.FC<dialogProps>=(props)=>{
                     {props.children}
                 </main>
                 <footer className={sc('footer')}>
-                    <Button type='theme' simple>
-                        取消
-                    </Button>
-                    <Button type='theme'>
-                        确认
-                    </Button>
+                    {props.buttons.map((item,index)=>
+                        React.cloneElement(item,{key:index})
+                    )}
                 </footer>
             </div>
         </>
@@ -37,4 +46,7 @@ const Dialog:React.FC<dialogProps>=(props)=>{
     )
 }
 
+Dialog.defaultProps={
+    closeOnClickMask:false
+}
 export default Dialog;
--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,15 +1,43 @@
 import React, {useState} from 'react';
 import Dialog from './Dialog';
+import {Button} from '../index';
 
 const DialogExample: React.FunctionComponent = () => {
     const [x,setX]=useState(false);
+    const [y,setY]=useState(false);
     return (
-        <div>
-            <button onClick={()=>setX(!x)}>button</button>
-            <Dialog visible={x}>
-                <div>hihihi</div>
-            </Dialog>
-        </div>
+        <>
+            <div>
+                <button onClick={()=>setX(!x)}>button</button>
+                <Dialog visible={x} buttons={
+                    [
+                        <Button type='theme' simple onClick={()=>setX(false)}>
+                            取消
+                        </Button>,
+                        <Button type='theme'  onClick={()=>setX(false)}>
+                            确认
+                        </Button>
+                    ]
+                } onClose={()=>setX(false)}>
+                    <div>peach&moon</div>
+                </Dialog>
+            </div>
+            <div>
+                <button onClick={()=>setY(!x)}>button</button>
+                <Dialog visible={y} buttons={
+                    [
+                        <Button type='theme' simple onClick={()=>setY(false)}>
+                            取消
+                        </Button>,
+                        <Button type='theme'  onClick={()=>setY(false)}>
+                            确认
+                        </Button>
+                    ]
+                } onClose={()=>setY(false)} closeOnClickMask={true}>
+                    <div>peach&moon</div>
+                </Dialog>
+            </div>
+        </>
     );
 };
 
